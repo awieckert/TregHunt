13c33d76b37ce69d4d129dfb67ef3c8d259cd40c
Adam Wieckert <adam.wieckert@ncontracts.com>
2021-02-07 13:34:38 -0600

    So fucking dumb, copy always, fuck you!

--- TregHunt/Program.cs
+++ TregHunt/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 
 namespace TregHunt
 {
@@ -6,7 +7,33 @@ namespace TregHunt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConfiguration Configuration = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .Build();
+
+            var wtf = Configuration.GetSection("FileLocation");
+            var filePath = GetUserFileLocation(Configuration);
+            var test = 2;
+        }
+
+        private static string GetUserFileLocation(IConfiguration config)
+        {
+            Console.WriteLine("Welcome to TregHunt! A pubmed searching utiliy.");
+            Console.WriteLine("You supply TregHunt with an excel spreadsheet of your custom search terms.");
+            Console.WriteLine("Please see the README at https://github.com/awieckert/TregHunt for instructions on how to use TregHunt");
+            Console.WriteLine("*******************************");
+            Console.WriteLine(@"Please provide your excel file with search terms. 
+                                You may either use the default excel file and location 
+                                or provide the path to your own excel file.");
+            Console.WriteLine("Default [D], or provide the full file path (C:\\Documents\\excelFile.xlsx)");
+
+            var userFilePath = Console.ReadLine();
+            if (userFilePath.Equals("d", StringComparison.OrdinalIgnoreCase))
+            {
+                return config["FileLocation"];
+            }
+
+            return userFilePath;
         }
     }
 }
