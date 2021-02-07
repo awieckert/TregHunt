e9a042a86e19ae80e753d13cd5a8d9041fb9bfdd
Adam Wieckert <adam.wieckert@ncontracts.com>
2021-02-07 11:46:19 -0600

    first commit

---
+++ TregHunt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TregHunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
